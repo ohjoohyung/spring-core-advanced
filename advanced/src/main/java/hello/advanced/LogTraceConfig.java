90dcc9b127b4bb9fe04afeca020bb801904ba409
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2022-01-14 16:23:15 +0900

    feat: 프록시 패턴, 데코레이터 패턴 추가

--- advanced/src/main/java/hello/advanced/LogTraceConfig.java
+++ advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -4,13 +4,15 @@ import hello.advanced.trace.logtrace.LogTrace;
 import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 
 @Configuration
 public class LogTraceConfig {
 
     @Bean
     //빈 스코프를 request로, LogTrace가 필요한 곳에 프록시 객체를 두도록 설정해도 동시성 문제 해결 가능
-//    @Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+    @Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
     public LogTrace logTrace() {
         return new ThreadLocalLogTrace();
     }
